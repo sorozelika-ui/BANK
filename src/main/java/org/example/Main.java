9ee35791a9b781bcb8c3dee4920edaecfb6a7d96
sorowagniguizelika <sorozelika@gmail.com>
2026-05-24 17:59:21 +0000

    création des instances de chque classe

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Création du compte courant
+        CheckingAccount checking =
+                new CheckingAccount("CHK001", 500000, 100000);
+
+        // Création du compte épargne
+        SavingAccount savings =
+                new SavingAccount("SVG001", 750000);
+
+        // Création du certificat de dépôt
+        COD cod =
+                new COD("COD001", 1000000);
     }
 }
